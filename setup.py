d1dc1130329b9339e96b2d0de7822473bb41d39b
Alex DeBrie <alexdebrie1@gmail.com>
2016-09-07 14:44:52 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="luigi-monitor",
-    version="0.2.3",
+    version="1.0.0",
     description="Send summary messages of your Luigi jobs to Slack.",
     url="https://github.com/hudl/luigi-monitor",
     author="Hudl",

dc3c9c540179518c84df0bdb7756c986428774e3
Dillon Stadther <dlstadther@gmail.com>
2018-08-16 07:15:58 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="luigi-monitor",
-    version="1.1.3",
+    version="1.1.4",
     description="Send summary messages of your Luigi jobs to Slack.",
     long_description=open("README.md").read(),
     url="https://github.com/hudl/luigi-monitor",
